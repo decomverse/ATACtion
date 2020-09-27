821d47d215ff219e74a5e5183d21dc14b1c11a15
sebastianpineda <sebastianpineda95@gmail.com>
2020-09-27 01:28:46 -0500

    fix rowRanges bug

--- R/linking.R
+++ R/linking.R
@@ -1,8 +1,8 @@
 compute_ATACtion_archSE <- function(ace, arch_slot = "unified_feature_specificity") {
 	GR = SummarizedExperiment::rowRanges(ace)
 	archs = rowMaps(ace)[[arch_slot]]
-	SE = SummarizedExperiment(assays = list(peaks = archs), SummarizedExperiment::rowRanges = GR)
-	
+	SE = SummarizedExperiment(assays = list(peaks = archs), rowRanges = GR)
+
 	return(SE)
 }
 
@@ -28,38 +28,38 @@ run_ATACtion_cicerify <- function(SE, assay_slot = "peaks") {
 	peak.labels = rownames(SE)
 	arch.labels = colnames(SE)
 	archs = as(assays(SE)[[assay_slot]], 'dgTMatrix')
-	
+
 	df = data.frame(Peak = peak.labels[archs@i+1], Cell = arch.labels[archs@j+1], Count = as.numeric(archs@x), stringsAsFactors = FALSE)
-	
+
 	arch.CDS = make_atac_cds(df, binarize = FALSE)
 	perm = match(paste("A", 1:ncol(arch.CDS), sep = ""), colnames(arch.CDS))
 	arch.CDS = arch.CDS[, perm]
-	
+
 	## Annotate with gene promoters for future reference (needed in build_gene_activity_matrix())
 	path = system.file(package="ATACtionDB", "data", sprintf('refGene_%s.rda', gn))
-	load(path)  
-	
-	
+	load(path)
+
+
 	promoter.bed = data.frame(chr = as.character(refGene@seqnames), start = as.numeric(refGene@ranges@start), end = as.numeric(refGene@ranges@start + refGene@ranges@width - 1), gene = refGene$Gene, stringsAsFactors = FALSE)
 	arch.CDS = annotate_cds_by_site(arch.CDS, promoter.bed)
-	
+
 	system.time( {conns <- run_cicero(arch.CDS, chr.table)} )
-	
+
 	out = list(conns = conns, arch.CDS = arch.CDS)
-	return(out)  
+	return(out)
 }
 
 compute_cicero_ccans <- function(conns) {
 	system.time( {CCAN_assigns <- generate_ccans(conns)} )
 
-	return(CCAN_assigns)	
+	return(CCAN_assigns)
 }
 
 
 compute_cicero_gene_expression <- function(arch.CDS, conns) {
 	require(cicero)
-	
-	
+
+
 	unnorm_ga <- build_gene_activity_matrix(arch.CDS, conns)
 
 	num_genes <- pData(arch.CDS)$num_genes_expressed
@@ -67,54 +67,54 @@ compute_cicero_gene_expression <- function(arch.CDS, conns) {
 	cicero_gene_activities <- as.matrix(normalize_gene_activities(unnorm_ga, num_genes))
 
 	return(cicero_gene_activities)
-}  
-  
+}
+
 cicero.to.GenomicInteractions <- function(ace, conns, coaccess_cutoff = 0.25) {
   GR = SummarizedExperiment::rowRanges(ace)
   values(GR) = c()
-  
-  # peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width-1, sep = '_')  
+
+  # peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width-1, sep = '_')
   peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width, sep = '_')
-  
+
   ii = match(conns$Peak1, peak.labels)
   jj = match(conns$Peak2, peak.labels)
   vv = conns$coaccess
   mask = !is.na(vv) & vv > coaccess_cutoff
-  
+
 
   anchor.one = GR[ii[mask]]
   anchor.two = GR[jj[mask]]
-  
+
   GR.interactions = GenomicInteractions::GenomicInteractions(anchor.one, anchor.two, coaccess=vv[mask])
-  
-  return(GR.interactions)  
+
+  return(GR.interactions)
 }
 
 
 GenomicInteractions.to.cicero <- function(ace, GI) {
   GR = SummarizedExperiment::rowRanges(ace)
   values(GR) = c()
-  
-  # peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width-1, sep = '_')  
+
+  # peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width-1, sep = '_')
   peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width, sep = '_')
 
 
   GI.conns = data.frame(Peak1 = peak.labels[GI@anchor1], Peak2 = peak.labels[GI@anchor2])
   GI.conns = cbind(GI.conns, as.data.frame(GI@elementMetadata))
-  
-  
+
+
   ii = match(conns$Peak1, peak.labels)
   jj = match(conns$Peak2, peak.labels)
   vv = conns$coaccess
   mask = !is.na(vv) & vv > coaccess_cutoff
-  
+
 
   anchor.one = GR[ii[mask]]
   anchor.two = GR[jj[mask]]
-  
+
   GR.interactions = GenomicInteractions::GenomicInteractions(anchor.one, anchor.two, coaccess=vv[mask])
-  
-  return(GR.interactions)  
+
+  return(GR.interactions)
 }
 
 
@@ -122,15 +122,15 @@ conn.to.peakConnectivity.mat <- function(ace, conns, coaccess_cutoff = 0.25) {
 	GR = SummarizedExperiment::rowRanges(ace)
 	values(GR) = c()
 
-	# peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width-1, sep = '_')  
+	# peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width-1, sep = '_')
 	peak.labels = paste(GR@seqnames, GR@ranges@start, GR@ranges@start+GR@ranges@width, sep = '_')
 
 	ii = match(conns$Peak1, peak.labels)
 	jj = match(conns$Peak2, peak.labels)
 	vv = conns$coaccess
 	mask = !is.na(vv) & vv > coaccess_cutoff
-	connectivity.map = Matrix::sparseMatrix(i = ii[mask], j = jj[mask], x = vv[mask], dims = c(length(GR), length(GR)))  	
-	
+	connectivity.map = Matrix::sparseMatrix(i = ii[mask], j = jj[mask], x = vv[mask], dims = c(length(GR), length(GR)))
+
 	return(connectivity.map)
 }
 
@@ -146,7 +146,7 @@ GI.to.peakConnectivity.mat <- function(ace, GI, maxgap = 100) {
 	diag(connectivity.map) = 0
 
 	return(connectivity.map)
-}	
+}
 
 
 extend.cisConnectome.usingConns <-function(ace, conns, coaccess_cutoff=0.25) {
@@ -157,11 +157,11 @@ extend.cisConnectome.usingConns <-function(ace, conns, coaccess_cutoff=0.25) {
 
   connectivity.map = conn.to.peakConnectivity.mat(ace, conns, coaccess_cutoff)
   cisConnectome.mat = rowMaps(ace)[["cisConnectome"]]
-  
+
   extended.cisConnectome = connectivity.map %*% cisConnectome.mat
-  
+
   return(extended.cisConnectome)
-}  
+}
 
 extend.cisConnectome.usingGI <-function(ace, GI, max_gap = 100) {
   if( !("cisConnectome" %in% names(metadata(ace))) ) {
@@ -171,9 +171,8 @@ extend.cisConnectome.usingGI <-function(ace, GI, max_gap = 100) {
 
   connectivity.map = GI.to.peakConnectivity.mat(ace, GI, max_gap)
   cisConnectome.mat = rowMaps(ace)[["cisConnectome"]]
-  
-  extended.cisConnectome = connectivity.map %*% cisConnectome.mat
-  
-  return(extended.cisConnectome)
-}  
 
+  extended.cisConnectome = connectivity.map %*% cisConnectome.mat
+
+  return(extended.cisConnectome)
+}
